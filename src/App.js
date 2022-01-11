cd2b33092d123021fc45a288c1240b96b2b563bd
Brian <brianekonz@gmail.com>
2022-01-11 15:23:56 -0300

    navBar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBarIndex from './Componentes/MyFunctionalComponents';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Proyecto React
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBarIndex />
     </div>
+    
   );
 }
 
---
+++ src/Componentes/MyFunctionalComponents/index.jsx
@@ -0,0 +1,22 @@
+import "./styles.css";
+
+const NavBarIndex = () => {
+   return <div>
+        <ul className="navBar">
+        <li>
+            <a href="">Home</a>
+        </li>
+        <li>
+            <a href="">Artistas</a>
+        </li>
+        <li>
+            <a href="">Entradas</a>
+        </li>
+        <li>
+            <a href="">Contacto</a>
+        </li>
+        </ul>
+    </div>
+}
+
+export default NavBarIndex
